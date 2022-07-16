e1721ee1384abf6dc456c4e92d3dcc7736393a12
poveden <jorgepoveda@gmail.com>
2022-07-16 03:17:48 +0200

    Add changelog tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,7 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Update solution to .NET 6.0
 
-## [1.0.0](https://github.com/poveden/ChromaWrapper/releases/tag/v1.0.0) — 2021-10-09
+## [1.0.0](https://github.com/poveden/ChromaWrapper/releases/tag/v1.0.0) â€” 2021-10-09
 
 ### Added
 
---
+++ test/Internal/MetaTestsCommon.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Xunit;
+
+namespace ChromaWrapper.Tests.Internal
+{
+    internal static class MetaTestsCommon
+    {
+        public static string GetSolutionDirectory()
+        {
+            const string SolutionFilename = "ChromaWrapper.sln";
+
+            var fi = new FileInfo(typeof(MetaTestsCommon).Assembly.Location);
+
+            for (var di = fi.Directory; di != null; di = di.Parent)
+            {
+                if (di.GetFiles(SolutionFilename, SearchOption.TopDirectoryOnly).Length != 0)
+                {
+                    return di.FullName;
+                }
+            }
+
+            throw new InvalidOperationException("Solution directory could not be found.");
+        }
+    }
+}
